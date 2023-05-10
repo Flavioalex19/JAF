20d2e47c932f3df2afd0a562e43a7ed06be394e1
FA77\flavi <flavioalex19@gmail.com>
2023-05-10 19:44:30 -0300

    Occluder and Terrain

--- Assets/Scripts/Camera/IsoCamera.cs
+++ Assets/Scripts/Camera/IsoCamera.cs
@@ -7,9 +7,18 @@ public class IsoCamera : MonoBehaviour
 {
     
     [SerializeField]Transform _playerTransform;//player transform
-    PlayerInput _playerInput;
+    [SerializeField]PlayerInput _playerInput;
     Transform _dialogueFocusTransform;
     [SerializeField]Camera _camera;
+
+    public static int AlphaClipping = Shader.PropertyToID("_Alpha");
+    public Material WallMaterial;
+    public Camera Camera;
+    public LayerMask LayerMask;
+
+    float amount = 0;
+    float increaseAmount = .9f;
+
 
     bool _isOnPlayer;
 
@@ -21,7 +30,34 @@ public class IsoCamera : MonoBehaviour
         _isOnPlayer = true;
     }
 
+    private void Update()
+    {
+        var dir = Camera.transform.position - transform.position;
+        Ray ray = new Ray(transform.position, dir.normalized);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, 3000f, LayerMask))
+        {
+            if (amount < 1)
+            {
+                WallMaterial.SetFloat(AlphaClipping, amount += increaseAmount * Time.deltaTime);
+            }
+            else WallMaterial.SetFloat(AlphaClipping, 1f);
 
+        }
+        else
+        {
+            if (amount >= 0)
+            {
+                WallMaterial.SetFloat(AlphaClipping, amount -= increaseAmount * Time.deltaTime);
+            }
+            else
+            {
+                WallMaterial.SetFloat(AlphaClipping, 0);
+                amount = 0;
+            }
+
+        }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
